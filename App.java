23a6e1d3ba194cda9d78c0a92b22d4d24f8a294e
abhi <abhishekb@heaptrace.com>
2025-09-22 17:38:13 +0530

    firsr commit:added 4line java message

---
+++ App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, this is my first Git commit");
+        System.out.println("Git is now set up on Linux");
+        System.out.println("This is assignment step 1");
+    }
+}
+
